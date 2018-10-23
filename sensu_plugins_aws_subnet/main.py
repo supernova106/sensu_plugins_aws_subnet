22ef123d5d0549e7fdf20367ca75ebaff6d15f66
Bean Nguyen <binhnguyen@ebates.com>
2018-10-23 14:18:57 +0800

    Add sensu plugin

--- sensu_plugins_aws_subnet/main.py
+++ sensu_plugins_aws_subnet/main.py
@@ -18,54 +18,90 @@ try:
 except Exception:
     logger.error("An error has occured. Cannot set logger!")
 
-def check_subnet():
-    """
-    check subnet
-    """
-    try:
-        threshold = int(os.environ['SUBNET_THRESHOLD'])
+class SubnetCheck(SensuPluginCheck):
+    def setup(self):
+        # Setup is called with self.parser set and is responsible for setting up
+        # self.options before the run method is called
 
-        if threshold >= 100 or threshold <= 0:
-            logger.warning(
-            "SUBNET_THRESHOLD indicates percentage of available IPs. It must be < 100 and > 0. Set to default value of 20")
-            threshold = 20
-    except KeyError:
-        logger.warning('SUBNET_THRESHOLD is not set. Set to default value of 20')
-        threshold = 20
-
-    filters = []
-    subnets = list(ec2.subnets.filter(Filters=filters))
-    data = {}
-    for subnet in subnets:
-        subnet_info = ec2.Subnet(subnet.id)
-        total_available_ips = ipaddress.ip_network(subnet_info.cidr_block).num_addresses - 5
-        if subnet_info.available_ip_address_count/total_available_ips*100 <= threshold:
-            if subnet_info.vpc_id not in data:
-                data[subnet_info.vpc_id] = []
-
-            data[subnet_info.vpc_id].append(
-                (subnet.id, subnet_info.available_ip_address_count, total_available_ips))
-
-    for vpc_id in data:
-        vpc_info = client.describe_vpcs(
-            VpcIds=[
-                vpc_id,
-            ]
+        self.parser.add_argument(
+            '-w',
+            '--warning',
+            default=20,
+            type=int,
+            help='warning threshold for percentage of subnet available IPs, default to 20'
+        )
+        self.parser.add_argument(
+            '-c',
+            '--critical',
+            default=10,
+            type=int,
+            help='critical threshold for percentage of subnet available IPs, default to 10'
+        )
+        self.parser.add_argument(
+            '-m',
+            '--message',
+            default=None,
+            help='Message to print'
         )
 
-        vpc_name = ""
-        for value in vpc_info['Vpcs']:
-            if 'Tags' in value:
-                for tag in value['Tags']:
-                    if tag['Key'] == 'Name':
-                        vpc_name = tag['Value']
+    def run(self):
+        # this method is called to perform the actual check
+        self.check_subnet() # defaults to class name
 
-        print("The following subnets are running out of ipv4 capacity:\n{}/{} ({})".format(region,
-            vpc_id, vpc_name))
-        for row in data[vpc_id]:
-            print("\t- {} has {} available IPs out of {}".format(row[0],row[1],row[2]))
+        if self.options.warning == 0:
+            self.ok(self.options.message)
+        elif self.options.warning == 1:
+            self.warning(self.options.message)
+        elif self.options.warning == 2:
+            self.critical(self.options.message)
+        else:
+            self.unknown(self.options.message)
+
+    def check_subnet(self):
+        """
+        check subnet
+        """
+        threshold = self.options.warning
+
+        if threshold >= 100 or threshold <= 0:
+            logger.warning("-w indicates percentage of available IPs. It must be < 100 and > 0. Set to default value of 20")
+            threshold = 20
+
+        filters = []
+        subnets = list(ec2.subnets.filter(Filters=filters))
+        data = {}
+        for subnet in subnets:
+            subnet_info = ec2.Subnet(subnet.id)
+            total_available_ips = ipaddress.ip_network(subnet_info.cidr_block).num_addresses - 5
+            if subnet_info.available_ip_address_count/total_available_ips*100 <= threshold:
+                if subnet_info.vpc_id not in data:
+                    data[subnet_info.vpc_id] = []
+
+                data[subnet_info.vpc_id].append(
+                    (subnet.id, subnet_info.available_ip_address_count, total_available_ips))
+
+        msg = []
+        for vpc_id in data:
+            vpc_info = client.describe_vpcs(
+                VpcIds=[
+                    vpc_id,
+                ]
+            )
+
+            vpc_name = ""
+            for value in vpc_info['Vpcs']:
+                if 'Tags' in value:
+                    for tag in value['Tags']:
+                        if tag['Key'] == 'Name':
+                            vpc_name = tag['Value']
+
+            msg.append("The following subnets are running out of ipv4 capacity:\n{}/{} ({})".format(region, vpc_id, vpc_name))
+            for row in data[vpc_id]:
+                msg.append("\t- {} has {} available IPs out of {}".format(row[0], row[1], row[2]))
+
+        self.options.message = "\n".join(msg)
+        logger.warning(self.options.message)
 
 if __name__ == "__main__":
-    check_subnet()
-
+    f = SubnetCheck()
 
