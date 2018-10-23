b3a764f00ffd6600654e2681a9c59fcfa0fab125
Bean Nguyen <binhnguyen@ebates.com>
2018-10-23 17:15:58 +0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,11 @@ setup(
     packages=['sensu_plugins_aws_subnet'],
     scripts=['bin/sensu-check-aws-subnet'],
     url='https://github.com/supernova106/sensu_plugins_aws_subnet',
-    license='LICENSE',
+    license='BSD',
+    python_requires='>=3',
     description='A extention from Python sensu plugins framework to check aws subnet available IPs',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'boto3',
         'ipaddress',
